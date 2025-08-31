07d72c2b0479a7a3d6ab26b50e03f5cfc29e1bb5
Ibrram <9iro@bk.ru>
2025-08-31 05:04:35 +0300

    init provider

---
+++ lib/core/providers/app_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppProvider extends ChangeNotifier {
+  // lang
+  Locale _locale = const Locale('en');
+  Locale get locale => _locale;
+  // theme
+  ThemeMode _theme = ThemeMode.light;
+  ThemeMode get theme => _theme;
+
+  void changeTheme(ThemeMode theme) {
+    _theme = theme;
+    notifyListeners();
+  }
+
+  void changeLang(String lang) {
+    _locale = Locale(lang);
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_task/core/providers/app_provider.dart';
 import 'package:news_app_task/core/routes/app_routes.dart';
 import 'package:news_app_task/core/routes/routes_name.dart';
 import 'package:news_app_task/core/theme_manager/app_theme_manager.dart';
+import 'package:provider/provider.dart';
 
 import 'l10n/app_localizations.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -14,14 +21,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AppProvider provider = Provider.of<AppProvider>(context);
     return MaterialApp(
       theme: AppThemeManager.light,
-      themeMode: ThemeMode.light,
+      themeMode: provider.theme,
       initialRoute: RoutesName.splash,
       onGenerateRoute: AppRoutes.onGenerateRoutes,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: const Locale('ar'),
+      locale: provider.locale,
     );
   }
 }
